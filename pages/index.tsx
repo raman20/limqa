9bbff501011571a6fb7571c5c0fce834f4da3034
raman dwivedi <ramandwivedi20@protonmail.com>
2024-11-10 14:43:36 +0530

    updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useState, useRef, useEffect } from 'react';
+import React, { FormEvent, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import { DNA } from 'react-loader-spinner';
 
@@ -14,6 +14,11 @@ export default function Home() {
     const newConversation = { question: query, answer: '' };
     setConversations((prev) => [...prev, newConversation]);
     setIsLoading(true);
+    const setAnswer = (answer: string) => setConversations((prev) => {
+      const updatedConversations = [...prev];
+      updatedConversations[updatedConversations.length - 1].answer = answer;
+      return updatedConversations;
+    });
 
     try {
       const res = await fetch(`/api/ask`, {
@@ -34,18 +39,10 @@ export default function Home() {
       while (!done) {
         ({ done, value } = await reader.read());
         answer += decoder.decode(value || new Uint8Array());
-        setConversations((prev) => {
-          const updatedConversations = [...prev];
-          updatedConversations[updatedConversations.length - 1].answer = answer;
-          return updatedConversations;
-        });
+        setAnswer(answer);
       }
     } catch (error) {
-      setConversations((prev) => {
-        const updatedConversations = [...prev];
-        updatedConversations[updatedConversations.length - 1].answer = '**Oops! Something went wrong. Please try again later.**';
-        return updatedConversations;
-      });
+      setAnswer('**Oops something went wrong!!**');
     } finally {
       setQuery('');
       setIsLoading(false);
@@ -55,14 +52,18 @@ export default function Home() {
   return (
     <div className="chat-container">
       <div className="chat-history">
+        {conversations.length ? <></> : <>
+          <h2 className="chat-heading">LimQA</h2>
+          <p className="tagline">Your questions, our answers!</p></>
+        }
         {conversations.map((conv, index) => (
           <div key={index} className="chat-message">
             <p className="question"><strong>You:</strong> {conv.question}</p>
             {
-              isLoading && index === conversations.length - 1 ? <DNA/> : 
-              <div className="answer">
-                <ReactMarkdown>{conv.answer}</ReactMarkdown>
-              </div>
+              isLoading && index === conversations.length - 1 ? <DNA /> :
+                <div className="answer">
+                  <ReactMarkdown>{conv.answer}</ReactMarkdown>
+                </div>
             }
           </div>
         ))}
@@ -73,7 +74,7 @@ export default function Home() {
           type="text"
           value={query}
           onChange={(e) => setQuery(e.target.value)}
-          placeholder="Ask anything... upto 50 chars"
+          placeholder="Ask anything... up to 50 chars"
           className="input"
           disabled={isLoading}
         />
